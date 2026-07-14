7d52a1bda890723e3385247137525969ba5e8ff3
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-14 14:11:46 +0100

    Update register.js

--- register.js
+++ register.js
@@ -4,59 +4,104 @@ import {
     createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
+
 import {
     doc,
     setDoc,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
+
 const form = document.getElementById("registerForm");
+
 
 form.addEventListener("submit", async (e) => {
 
     e.preventDefault();
 
-    const fullname = document.getElementById("fullname").value.trim();
-    const username = document.getElementById("username").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const country = document.getElementById("country").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
-    const referral = document.getElementById("referral").value.trim();
 
-    if (password !== confirmPassword) {
+    const fullname = document
+    .getElementById("fullname")
+    .value
+    .trim();
+
+
+    const email = document
+    .getElementById("email")
+    .value
+    .trim();
+
+
+    const password = document
+    .getElementById("password")
+    .value;
+
+
+    const confirmPassword = document
+    .getElementById("confirmPassword")
+    .value;
+
+
+
+    if(password !== confirmPassword){
+
         alert("Passwords do not match.");
+
         return;
+
     }
 
-    try {
 
-        const userCredential =
-            await createUserWithEmailAndPassword(auth, email, password);
+
+    try{
+
+
+        const userCredential = 
+        await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
 
         const user = userCredential.user;
 
-        await setDoc(doc(db, "users", user.uid), {
 
-            fullname,
-            username,
-            email,
-            country,
-            referral,
-            wallet: 0,
-            currency: "NGN",
-            createdAt: serverTimestamp()
 
-        });
+        await setDoc(
+            doc(db,"users",user.uid),
+            {
 
-        alert("Account created successfully!");
+                fullname,
 
-        window.location.href = "dashboard.html";
+                email,
 
-    } catch (error) {
+                wallet:0,
+
+                currency:"NGN",
+
+                createdAt:serverTimestamp()
+
+            }
+        );
+
+
+
+        alert("FameSMS account created successfully!");
+
+
+
+        window.location.href="dashboard.html";
+
+
+
+    }catch(error){
+
 
         alert(error.message);
 
+
     }
+
 
 });
