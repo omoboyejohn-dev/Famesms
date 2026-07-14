5db4d1b716cfb3f992f0be4c494a4a0256a43d01
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-14 13:29:52 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,45 @@
+// Firebase SDK
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+// Firebase Configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD3ubMdCyz8FrXFpBGJNz7nHuCxAEYqI9w",
+
+    authDomain: "famesms-23e17.firebaseapp.com",
+
+    projectId: "famesms-23e17",
+
+    storageBucket: "famesms-23e17.firebasestorage.app",
+
+    messagingSenderId: "749215030285",
+
+    appId: "1:749215030285:web:0453bb888eba85c2e4c944",
+
+    measurementId: "G-F1YKMVT827"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+// Services
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+// Export
+
+export { auth, db };
