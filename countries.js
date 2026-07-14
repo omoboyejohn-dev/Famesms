e1b8ddcec7e8509ba4f568b8728200d9150ff608
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-14 20:52:02 +0100

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,69 @@
+import { auth } from "./firebase.js";
+
+const countries = [
+
+    { name: "🇲🇽 Mexico" },
+    { name: "🇧🇷 Brazil" },
+    { name: "🇪🇸 Spain" },
+    { name: "🇮🇳 India" },
+    { name: "🇨🇦 Canada" },
+    { name: "🇺🇸 USA" },
+    { name: "🇮🇩 Indonesia" },
+    { name: "🇬🇧 England" },
+    { name: "🇵🇭 Philippines" },
+    { name: "🇿🇦 South Africa" }
+
+];
+
+const countriesGrid = document.getElementById("countriesGrid");
+const countrySearch = document.getElementById("countrySearch");
+
+function displayCountries(list){
+
+    countriesGrid.innerHTML = "";
+
+    list.forEach(country => {
+
+        countriesGrid.innerHTML += `
+
+        <div class="card">
+
+            <h3>${country.name}</h3>
+
+            <button
+                class="buy-btn"
+                onclick="openCountry('${country.name}')">
+
+                View Services
+
+            </button>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+displayCountries(countries);
+
+countrySearch.addEventListener("input", () => {
+
+    const keyword = countrySearch.value.toLowerCase();
+
+    const filtered = countries.filter(country =>
+        country.name.toLowerCase().includes(keyword)
+    );
+
+    displayCountries(filtered);
+
+});
+
+window.openCountry = function(country){
+
+    localStorage.setItem("selectedCountry", country);
+
+    window.location.href = "country-services.html";
+
+};
